09d3ba8b47e260a18511f0c4a786723cb4d11a35
Kimbsy <lordkimber@gmail.com>
2019-01-01 16:23:16 +0000

    initial commit, adding liverpool, basic wld and deck matching

---
+++ src/mtg_results/core.clj
@@ -0,0 +1,55 @@
+(ns mtg-results.core
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+(defn get-all-tournaments
+  []
+  (let [dir (-> "results"
+                io/resource
+                io/file
+                file-seq)
+        files (filter #(.isFile %) dir)]
+    (->> files
+         (map slurp)
+         (map edn/read-string))))
+
+(defn get-tournaments-by-deck-url
+  [url]
+  (filter (fn [{:keys [deck]}]
+            (= url (:url deck)))
+          (get-all-results)))
+
+(defn match-wld-rate-for-day
+  [totals day]
+  (reduce (fn [totals {:keys [result] :as m}]
+            (update totals result inc))
+          totals
+          (:matches day)))
+
+(defn match-wld-rate-for-tournament
+  [totals tournament]
+  (reduce match-wld-rate-for-day
+          totals
+          (:results tournament)))
+
+(defn match-wld-rate
+  [tournaments]
+  (reduce match-wld-rate-for-tournament
+          {:win 0
+           :loss 0
+           :draw 0}
+          tournaments))
+
+(match-wld-rate (get-all-tournaments))
+
+
+
+
+
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/mtg_results/core_test.clj
@@ -0,0 +1,7 @@
+(ns mtg-results.core-test
+  (:require [clojure.test :refer :all]
+            [mtg-results.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
